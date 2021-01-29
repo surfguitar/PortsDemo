804e2422de2dbab9ba5c9feabcce302da1fe7c41
Gustav Wilhelmsson <Gustav.wilhelmsson@bokadoktorn.se>
2021-01-29 11:05:44 +0100

    Get without DB. Dummy data.

--- PortAndAdapterDemo/05 - DataSideAdapter/BookingPersistence/BookingSqlAdapter/BookingRepository.cs
+++ PortAndAdapterDemo/05 - DataSideAdapter/BookingPersistence/BookingSqlAdapter/BookingRepository.cs
@@ -43,25 +43,37 @@ namespace BookingSqlAdapter
 
         public IEnumerable<Booking> GetBookings()
         {
-            const string sql = @"SELECT B.*, HI.Id AS HealthId, HI.*, P.Id AS PersonId, P.* FROM Booking B, HealthInstitution HI, Person P
-                                WHERE B.HealthInstitutionId = HI.Id
-                                AND B.PersonId = P.Id";
+            //const string sql =
+            //    @"SELECT B.*, HI.Id AS HealthId, HI.*, P.Id AS PersonId, P.* FROM Booking B, HealthInstitution HI, Person P
+            //                    WHERE B.HealthInstitutionId = HI.Id
+            //                    AND B.PersonId = P.Id";
 
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-                
-                var records = connection.Query(sql);
+            //using (var connection = new SqlConnection(_connectionString))
+            //{
+            //    connection.Open();
 
-                var bookingsList = new List<Booking>();
+            //    var records = connection.Query(sql);
 
-                foreach (var record in records)
-                {
-                    bookingsList.Add(new Booking(record.Id, record.AppointmentTime, new HealthInstitution(record.HealthId, record.InstitutionName, record.Address, record.Phone, record.CreatedAt), new Person(record.PersonId, record.Name, record.Email)));
-                }
+            //    var bookingsList = new List<Booking>();
 
-                return bookingsList;
-            }
+            //    foreach (var record in records)
+            //    {
+            //        bookingsList.Add(new Booking(record.Id, record.AppointmentTime,
+            //            new HealthInstitution(record.HealthId, record.InstitutionName, record.Address, record.Phone,
+            //                record.CreatedAt), new Person(record.PersonId, record.Name, record.Email)));
+            //    }
+
+            //    return bookingsList;
+            //}
+
+            var bookingsList = new List<Booking>();
+
+            bookingsList.Add(new Booking(Guid.Parse("9e57aaee-dd6e-4e34-8285-228c649a4f7d"), DateTime.UtcNow,
+                                new HealthInstitution(Guid.Parse("1ce7dc77-5e3b-4ba0-a0e4-0d25b23a9c11"), "Slottet", "skärgatan 777",
+                                "08-66666", DateTime.UtcNow),
+                                new Person(Guid.Parse("f27c6b18-2853-4250-af1e-9e4371a1b12a"), "Jörgen Katz", "jorgen@katz.com")));
+
+            return bookingsList;
         }
 
         public async Task UpdateBooking(Booking booking)
@@ -72,9 +84,7 @@ namespace BookingSqlAdapter
             {
                  await connection.OpenAsync();
 
-
-
-                await connection.ExecuteAsync(sql,
+                 await connection.ExecuteAsync(sql,
                      new
                      {
                          Id = booking.Id,
